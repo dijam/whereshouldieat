e6cc1b4db94b6f624d6ad2c7933ba4cdbd3c8423
Dijam <majid.sadeghi@gmail.com>
2013-02-16 18:50:37 +0100

    Random showing and next btn

--- main.js
+++ main.js
@@ -10,26 +10,18 @@ var Main = function(){
 	this.init();
 }
 
-function setGoogleMap(){
-	console.log("Latitude: " + window.position.coords.latitude);
-	console.log("Longitude: " + window.position.coords.longitude);
-
+function setGoogleMap(position){
 	var mapOptions = {
-		center: new google.maps.LatLng(window.position.coords.latitude, window.position.coords.longitude),
+		center: new google.maps.LatLng(position.Ya, position.Za),
 		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
 		mapOptions);
+	return map;
 
-	var myLocation = new google.maps.LatLng(window.position.coords.latitude, window.position.coords.longitude);
-	var myloc = new google.maps.Marker({
-    	map: map,
-    	position: myLocation,
-    	title: "You are here!"
-	});
-
+	// new google.maps.LatLng(window.position.coords.latitude, window.position.coords.longitude);
 }
 
 function setLocation() {
@@ -42,9 +34,14 @@ function setLocation() {
 }
 
 function setPosition(position) {
-	window.position = position;
-	setGoogleMap();
-	new LocationFinder(position);
+	// console.log("Your location: " + position);
+	// window.position = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);  //Uncomment to enable auto location finder
+
+	window.position = new google.maps.LatLng(59.34702, 18.040195); // Uncomment for manual location
+	window.map = setGoogleMap(window.position);
+
+	createMarker(window.position, "Your Location", "http://maps.google.com/mapfiles/ms/icons/green-dot.png");
+	new LocationFinder(window.position, window.map);
 }
 
 
@@ -65,45 +62,69 @@ function showError(error) {
 	}
 }
 
-var LocationFinder = function (position){
-	var http = require('http');
-
-	var url = "https://maps.googleapis.com/maps/api/place/add/json?sensor=true&key=AIzaSyAFoZAd_6SBPJBMXybVDknm0UjsTO5A0ps HTTP/1.1";
-	http.request()
-
-
-	var body = JSON.stringify({
-		"location": {
-			"lat": position.coords.latitude,
-			"lng": position.coords.longitude
-		},
-		"accuracy": 50,
-		"name": "Google Shoes!",
-		"types": ["restaurant"],
-		"language": "en-US"
+function createMarker(location, title, icon){
+	window.marker = new google.maps.Marker({
+    	map: window.map,
+    	position: location,
+    	icon: icon,
+    	animation: google.maps.Animation.DROP,
+    	title: title
 	});
+}
 
-	var options = {
-	    hostname: "maps.googleapis.com",
-	    port: 80,
-	    path: "/maps/api/place/add/json?sensor=true&key=AIzaSyAFoZAd_6SBPJBMXybVDknm0UjsTO5A0ps HTTP/1.1",
-	    method: "POST",
-	    headers: {
-	        "Content-Type": "application/json",
-	        "Content-Length": body.length
-	    }
+var LocationFinder = function (position, map){
+	var request = {
+    	location: new google.maps.LatLng(position.Ya, position.Za),
+    	radius: '500',
+    	types: ['restaurant', 'cafe', 'bar', 'food', 'restaurang']
 	};
 
-	var request = new http.request(options, function(res){
-		res.setEncoding('utf8');
-		request.on('data', function(chunk) {
-			console.log('chunk: ' + chunk);
-		});
-	});
 
-	request.on('error', function(e) {
-		console.log('problem with request: ' + e.message);
-	});
+	service = new google.maps.places.PlacesService(window.map);
+	service.nearbySearch(request, callbackPlaces);
+}
 
-	request.end(body)
+function callbackPlaces(results, status){
+	window.places = new Array();
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		console.log("Places found: " + results.length);
+		for (var i = 0; i < results.length; i++) {
+			window.places.push(results[i]);
+		}
+		fisherYates(window.places);
+		showLocation(window.places[0]);
+		window.count = 0;
+	} else {
+		console.log("No results");
+	}
+}
+
+function showLocation(location){
+	console.log(location); 
+	//create google location and show it on the map! See you after concert
+	createMarker(location.geometry.location, location.name);
+	var placeHTML = document.getElementById("place");
+	placeHTML.innerHTML = location.name;
+}
+
+function showNext(){
+	window.marker.setMap(null);
+	window.count ++;
+	if (window.count >= window.places.length)
+		window.count = 0;
+	showLocation(window.places[window.count]);
+}
+
+
+// Randomize restaurants' places
+function fisherYates(myArray){
+	var i = myArray.length;
+	if (i == 0) return false;
+	while (--i){
+		var j = Math.floor(Math.random() * (i + 1));
+		var tempi = myArray[i];
+		var tempj = myArray[j];
+		myArray[i] = tempj;
+		myArray[j] = tempi;
+	}
 }
